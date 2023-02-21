d8afff80bd9caacf2f8aaf2c6ee976a66b51eb8e
resourceldg <resourcelg@gmail.com>
2023-02-21 15:48:54 -0300

    tres puntos en escritores

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,14 +1,5 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 400;
-		margin: 0 0 0.5em 0;
-	}
+	
 
 	figure {
 		margin: 0 0 1em 0;
@@ -25,11 +16,6 @@
 		padding-bottom: 1em;
 	}
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
@@ -40,4 +26,4 @@
 <figure class=" xs:w-1/2 flex flex-wrap items-center justify-center">
 	<img alt='Editorial Jarilla' src='logo.svg'>
 </figure>
-<p class="link text-base font-serif italic md:text-lg"><a href="about"> “ Escritores trabajando para Escritores ”</a></p>
+<p class="link text-base font-serif italic md:text-lg"><a href="about"> “ Escritores trabajando para Escritores...”</a></p>
--- frontend/src/routes/thanks.svelte
+++ frontend/src/routes/thanks.svelte
@@ -1,1 +1,29 @@
-<h2>gracias!!!</h2>
+<style>
+	
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+		padding-bottom: 1em;
+	}
+
+</style>
+
+<svelte:head>
+	<title>Gracias</title>
+</svelte:head>
+
+
+<figure class=" xs:w-1/2 flex flex-wrap items-center justify-center">
+	<img alt='Editorial Jarilla' src='logo.svg'>
+</figure>
+<p class="link text-base font-serif italic md:text-lg"><a href="about"> “ Escritores trabajando para Escritores ”</a></p>
