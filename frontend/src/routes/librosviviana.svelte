2c442d301b08d1bbda57660eda6d2ebef994c526
resourceldg <32602897+resourceldg@users.noreply.github.com>
2023-03-13 12:04:19 -0300

    Update librosviviana.svelte

--- frontend/src/routes/librosviviana.svelte
+++ frontend/src/routes/librosviviana.svelte
@@ -16,7 +16,7 @@
           </div>
           <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Lop-Lop</h2>
           <p class="text-base leading-relaxed mt-2">Lop-Lop muestra la presencia de la Comunidad Huarpe Cuyana dentro de la diversidad de Pueblos Originarios que habitan nuestro país, en toda su extensión, haciendo conocer su pasado y su presente, mostrando la igualdad con nuestros hermanos, TODOS HIJOS DE UNA MISMA MADRE TIERRA.</p>
-          <a class="text-red-500 inline-flex items-center mt-3" href="start">Descargar
+          <a class="text-red-500 inline-flex items-center mt-3" href="https://mpago.la/1GmzBf3">Descargar
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -24,11 +24,11 @@
         </div>
         <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
           <div class="rounded-lg h-64 overflow-hidden">
-            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1204x504">
+            <img alt="content" class="object-cover object-center h-full w-full" src="TAPAS ROMPER CADENAS DIGITAL.JPG">
           </div>
-          <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Título</h2>
-          <p class="text-base leading-relaxed mt-2">Descripción</p>
-          <a class="text-red-500 inline-flex items-center mt-3" href="start">Leer más
+          <h2 class="text-xl font-medium title-font text-gray-900 mt-5">Romper Cadenas</h2>
+          <p class="text-base leading-relaxed mt-2">Esta obra, tiene tiene la triste tarea de llevar a forma literaria, casos reales de violencia de género y de femicidios</p>
+          <a class="text-red-500 inline-flex items-center mt-3" href="https://mpago.la/2MDFuWG">Descargar
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
